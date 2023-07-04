bccd39bce736896a908c909d6aae0a3e7b8865cd
dnjsxoghd <feelchok1234@gmail.com>
2023-07-05 00:33:24 +0900

    Java Bean Plus

--- spring-begin/src/main/java/com/sam/springbegin/App02HelloSpring.java
+++ spring-begin/src/main/java/com/sam/springbegin/App02HelloSpring.java
@@ -2,23 +2,23 @@ package com.sam.springbegin;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
 public class App02HelloSpring {
 
     public static void main(String[] args) {
 
         // 이 애플리케이션을 실행하면 JVM이 시작되며 SPRING 컨텍스트를 만들어보자.
 
-        
-        //Spring context 실행단계 jvm내에서 스프링 컨텍스트 만들기!!
-       var context = new AnnotationConfigApplicationContext(HelloWorldConfigration.class);
+        // Spring context 실행단계 jvm내에서 스프링 컨텍스트 만들기!!
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfigration.class);
 
-        //Spring Framework가 관리하도록 컨텍스트 빈 객체 가져오기
-        System.out.println(context.getBean("name")); 
+        // Spring Framework가 관리하도록 컨텍스트 빈 객체 가져오기
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address"));
+
         // 컨텍스트가 getBean이고 컨텍스트에서 값을 가져올 수 있는 방법 중 빈의 이름을 사용하는 방법
-        
-        
-    
-}
+
+    }
 
 }
--- spring-begin/src/main/java/com/sam/springbegin/HelloWorldConfigration.java
+++ spring-begin/src/main/java/com/sam/springbegin/HelloWorldConfigration.java
@@ -3,12 +3,31 @@ package com.sam.springbegin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age){};
+record Address(int row, String city){}
+
 @Configuration //클래스가 하나 이상의 Bean 메서드를 선언함을 나타낸다. 이곳에서 spring bean을 정의한다.
 public class HelloWorldConfigration {
 
     @Bean // bean을 생성함을 나타내는 어노테이션
     public String name(){
         return "Ranga";
+    }
+
+    @Bean
+    public int age(){
+        return 15;
+    }
+
+    @Bean
+    public Person person(){
+        
+        return new Person("Ravi", 20);
+    }
+
+    @Bean
+    public Address address(){
+        return new Address(20, "New YORK");
     }
 
     // 여기서 만드는 빈은 스프링 컨텍스트가 관리한다.
