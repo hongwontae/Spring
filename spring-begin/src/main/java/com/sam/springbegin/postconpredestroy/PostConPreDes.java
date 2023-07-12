4825b8ad6352613dfafa1bfc85e04f88d194f1d0
dnjsxoghd <feelchok1234@gmail.com>
2023-07-13 02:06:40 +0900

    init

---
+++ spring-begin/src/main/java/com/sam/springbegin/postconpredestroy/PostConPreDes.java
@@ -0,0 +1,53 @@
+package com.sam.springbegin.postconpredestroy;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class SomeClass{
+    private SomeDependency someDependency;
+    public SomeClass(SomeDependency someDependency){
+        this.someDependency=someDependency;
+        System.out.println("All dependencies are ready");
+    }
+    @PostConstruct
+    public void initialize(){
+        someDependency.getReady();
+    }
+    @PreDestroy
+    public void cleanUp(){
+        System.out.println("Clean UP");
+    }
+}
+
+@Component
+class SomeDependency{
+
+    public void getReady() {
+        System.out.println("Some logic using SomeDependency");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class PostConPreDes {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(PostConPreDes.class);
+        {
+
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+
+        }
+
+    }
+
+}
